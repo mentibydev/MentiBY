a8736e493f4727117933a5280432adcbeef396bf
techSaswata <saswata.24bcs10248@sst.scaler.com>
2025-06-12 11:44:47 +0530

    Dynamic Variables

---
+++ main.js
@@ -0,0 +1,12 @@
+// main.js
+
+// Top Banner Variables
+const upcomingCohortType = 'Basic';             // Basic or Placement - Important Section
+const upcomingCohortNum = '3.0';                // 3.0 or 4.0
+const upcomingCohortDate = 'July - 01';         // Date in format of Month - Day
+
+
+// Google Form or Cohort Link
+const basicCohortLink = 'https://mentiby.com/product/mentibycohort3'; 
+const placementCohortLink = 'https://docs.google.com/forms/d/e/1FAIpQLSdzPLrIql7qphbLxwB6tRcG7B4lg3QU4fYvha6-ya6wAQSBZg/viewform';
+const waitlistLink = 'https://docs.google.com/forms/d/e/1FAIpQLSdzPLrIql7qphbLxwB6tRcG7B4lg3QU4fYvha6-ya6wAQSBZg/viewform';
